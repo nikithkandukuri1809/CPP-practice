9e5cdf4fb37e6ec3ebaee56e6884079b73b9a40f
Nikith sai kandukuri <nikithkandukuri@gmail.com>
2026-03-06 17:20:50 +0530

    Create odd_or_even_(if,else).cpp

---
+++ odd_or_even_(if,else).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number :";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"given number is even ."<<endl;
+    }
+    else
+    {
+        cout<<"given number is odd ."<<endl;
+    }
+    return 0;
+    
+}
